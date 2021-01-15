785d62deac2ca2db9a9478c5b6534011d15c1359
ToshimiGunji <toshimi0218@icloud.com>
2021-01-16 01:27:44 +0900

    初期バオジョン完成

--- rsm.py
+++ rsm.py
@@ -2,18 +2,23 @@ import json
 import glob
 import sys
 from pprint import pprint
+import statistics
 from natsort import natsorted
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 pd.set_option('display.max_columns', 10)  # 10個結果を表示
 SUBSET_FEATURE = 10  # 部分空間の次元数
-CLASSIFIER_NUM = 1  # 分類器の数
+CLASSIFIER_NUM = 100  # 分類器の数
 gl_files = natsorted(glob.glob("./feature_sets/gallery/*"))  # ギャラリーデータファイル
 pr_files = natsorted(glob.glob("./feature_sets/probe/*"))  # プロ―ブデータファイル
 labels = []  # 正解ラベル
 predicted_labels = []  # 弱い分類器の予測ラベル格納
+final_labels = []
+# answer_labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]  # predicted_labelsの正解
+answer_labels = [0, 1]  # predicted_labelsの正解
 gl_person_num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]  # ギャラリーのデータ数
 pr_person_num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]  # プローブのデータ数
 gallery_feature = pd.DataFrame()  # ギャラリーの特徴量格納
@@ -49,22 +54,29 @@ probe_feature = probe_feature.reset_index(drop=True)
 
 # 分類器の数実行
 for index in pr_person_num:  # プローブの人数分
-    for l in range(CLASSIFIER_NUM):  # 部分空間数
+    predicted_labels = []  # ラベルリストを初期化
+    for classifier in range(CLASSIFIER_NUM):  # 部分空間数
         # ランダムに特徴量を取得
         gl_selected_feature = gallery_feature.sample(n=10, axis=1)
         print("This is gallery data\n", gl_selected_feature)
-
         # 対象のプローブデータを取得
         pr_selected_feature = pd.DataFrame(probe_feature[list(gl_selected_feature.columns)])
         pr_selected_feature = pr_selected_feature.loc[[int(index)]]
-        print("\nhere is a selected data\n", pr_selected_feature)
+        print("\nThis is probe selected data\n", pr_selected_feature)
 
         # 分類を実行
         subset_classifier = KNeighborsClassifier(n_neighbors=1, p=1)
         subset_classifier.fit(gl_selected_feature, labels)
         print("This data is classified as {0} \n".format(subset_classifier.predict(pr_selected_feature)))
         predicted_labels.extend(subset_classifier.predict(pr_selected_feature))
-        print("this is the predicted labels", predicted_labels)
-        print("\n\n")
+        print("this is weak classifier labels", predicted_labels)
+        if classifier == 4:
+            break
+    final_labels.append(statistics.mode(predicted_labels))
+    print("this is the answer{0}\n\n".format(final_labels))
     if index == "1":
         break
+
+# 正答率を算出
+# recognition_score = accuracy_score(final_labels, answer_labels)*100
+# print("The recognition score is {0}".format(recognition_score))
